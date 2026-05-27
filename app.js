73522e38f143c92b7a15fae0643a07914bd60e94
JonathanPageUTP <143666600+JonathanPageUTP@users.noreply.github.com>
2026-05-27 00:46:10 +0000

    pruebas de store y user

--- app.js
+++ app.js
@@ -5,11 +5,16 @@ const session      = require('express-session');
 const cookieParser = require('cookie-parser');
 const ejsLayouts   = require('express-ejs-layouts');
 const sequelize    = require('./config/database');
- const { Product, Order, OrderItem } = require('./models');
+const { Product, Order, OrderItem } = require('./models');
 
 const productRoutes  = require('./routes/products');
 const cartRoutes     = require('./routes/cart');
 const checkoutRoutes = require('./routes/checkout');
+
+const storeAuthRoutes = require('./routes/storeAuth');
+const { attachLocals } = require('./middleware/authMiddleware');
+
+const userAuthRoutes = require('./routes/userAuth');
 
 const app  = express();
 const port = process.env.PORT || 3000;
@@ -29,6 +34,8 @@ app.use(session({
   saveUninitialized: false,
   cookie: { maxAge: 3600000 }
 }));
+
+app.use(attachLocals);
 // Middleware: carrito vacio en sesion si no existe
 app.use((req, res, next) => {
   if (!req.session.cart) {
@@ -37,7 +44,11 @@ app.use((req, res, next) => {
   res.locals.cartItemCount = req.session.cart.totalQty || 0;
   next();
 });
-
+app.use(['/store/login', '/store/register',
+         '/user/login',  '/user/register',
+         '/store-admin', '/customer'],
+  (req, res, next) => { res.locals.layout = false; next(); }
+);
 /*app.get('/', (req, res) => {
   res.send(`
     Hello World - [REEMPLAZAR POR SU NOMBRE]
@@ -49,7 +60,8 @@ app.use((req, res, next) => {
 app.use('/',         productRoutes);
 app.use('/cart',     cartRoutes);
 app.use('/checkout', checkoutRoutes);
-
+app.use('/store', storeAuthRoutes);
+app.use('/user', userAuthRoutes);
 app.use((req, res) => {
   res.status(404).render('404', { title: 'Pagina no encontrada' });
 });
